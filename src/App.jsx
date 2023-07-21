8c5ec3a54e0dd58d875e22f72ba3cad5b7d064e1
j0srisk <j0srisk@gmail.com>
2023-07-21 11:02:38 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
