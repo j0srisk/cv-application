1c2d497445989e523325b743f01b18dd4372464d
j0srisk <j0srisk@gmail.com>
2023-07-22 21:02:57 -0500

    switched to rendering resumes in html

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-function Editor({ basicInfo, setBasicInfo}) {
+function Editor({ basicInfo, setBasicInfo, summary, setSummary}) {
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -9,62 +9,74 @@ function Editor({ basicInfo, setBasicInfo}) {
         });
     };
 
-
     return(
-    <div className="flex flex-1 flex-col bg-white rounded-lg shadow-lg">
-        <div className="flex flex-col">
-            <label htmlFor="firstName">First Name</label>
-            <input
-                type="text"
-                name="firstName"
-                value={basicInfo.firstName}
-                onChange={handleChange}
-            />
+    <div className="flex flex-1 flex-col bg-white rounded-lg shadow-lg p-8 gap-8">
+        <div className="flex flex-col shadow-lg rounded-lg border-slate-300 border-2 p-4 gap-4">
+            <h1 className="text-3xl font-bold">Basic Info</h1>
+            <div className="flex flex-col gap-2">
+                <div className="flex flex-col">
+                    <label className="text-lg font-bold" htmlFor="firstName">First Name</label>
+                    <input
+                        className="text-lg border-slate-300 border-2 rounded-lg bg-slate-100 p-1"
+                        type="text"
+                        name="firstName"
+                        value={basicInfo.firstName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-lg font-bold" htmlFor="lastName">Last Name</label>
+                    <input
+                        className="text-lg border-slate-300 border-2 rounded-lg bg-slate-100 p-1"
+                        type="text"
+                        name="lastName"
+                        value={basicInfo.lastName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-lg font-bold" htmlFor="email">Email</label>
+                    <input
+                        className="text-lg border-slate-300 border-2 rounded-lg bg-slate-100 p-1"
+                        type="text"
+                        name="email"
+                        value={basicInfo.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-lg font-bold" htmlFor="phone">Phone</label>
+                    <input
+                        className="text-lg border-slate-300 border-2 rounded-lg bg-slate-100 p-1"
+                        type="text"
+                        name="phone"
+                        value={basicInfo.phone}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-lg font-bold" htmlFor="social">Social</label>
+                    <input
+                        className="text-lg border-slate-300 border-2 rounded-lg bg-slate-100 p-1"
+                        type="text"
+                        name="social"
+                        value={basicInfo.social}
+                        onChange={handleChange}
+                    />  
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-                type="text"
-                name="lastName"
-                value={basicInfo.lastName}
-                onChange={handleChange}
-            />
-        </div>
-        <div className="flex flex-col">
-            <label htmlFor="email">Email</label>
-            <input
-                type="text"
-                name="email"
-                value={basicInfo.email}
-                onChange={handleChange}
-            />
-        </div>
-        <div>
-            <label htmlFor="phone">Phone</label>
-            <input
-                type="text"
-                name="phone"
-                value={basicInfo.phone}
-                onChange={handleChange}
-            />
-        </div>
-        <div className="flex flex-col">
-            <label htmlFor="social">Social</label>
-            <input
-                type="text"
-                name="social"
-                value={basicInfo.social}
-                onChange={handleChange}
-            />  
-        </div>
-        <div className="flex flex-col">
-            <label htmlFor="summary">Summary</label>
-            <textarea
-                type="text"
-                name="summary"
-                value={basicInfo.summary}
-                onChange={handleChange}
-            />
+        <div className="flex flex-col shadow-lg rounded-lg border-slate-300 border-2 p-4 gap-4">
+            <h1 className="text-3xl font-bold">Summary</h1>
+            <div className="flex flex-col">
+                <textarea
+                    className="text-lg border-slate-300 border-2 rounded-lg bg-slate-100 p-1 resize-y"
+                    type="text"
+                    name="summary"
+                    value={summary.summary}
+                    onChange={handleChange}
+                />
+            </div>
         </div>
     </div>
     )
--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,12 +1,39 @@
 /* eslint-disable react/prop-types */
-import ResumePDF from "./ResumePDF";
+import { useRef, useState, useEffect } from 'react';
 
 function Previewer({ basicInfo }) {
+    const [horizontalScale, setHorizontalScale] = useState(1);
+    const windowHeight = useRef(window.innerHeight);
+
+    // Function to calculate the horizontalScale based on window dimensions
+    const calculateHorizontalScale = () => {
+        const newHorizontalScale = (windowHeight.current - 40) / 1056;
+        setHorizontalScale(newHorizontalScale);
+    };
+
+    useEffect(() => {
+        // Event listener to handle window resize
+        const handleWindowResize = () => {
+          windowHeight.current = window.innerHeight;
+          calculateHorizontalScale();
+        };
+
+        // Calculate the initial horizontalScale once after the initial render
+        calculateHorizontalScale(); 
+        window.addEventListener('resize', handleWindowResize);
+    
+        // Cleanup the event listener when the component unmounts
+        return () => {
+          window.removeEventListener('resize', handleWindowResize);
+        };
+      }, []);
+
 
     return(
-    <div className="flex relative aspect-[7.8/11]">
-         <ResumePDF basicInfo={basicInfo}/>
-         <div className="absolute border-[1rem] border-white w-full h-full rounded-lg shadow-lg"></div>
+    <div className="aspect-[8.5/11] rounded-lg bg-white shadow-lg">
+        <div  style={{ transform: `scale(${horizontalScale.toFixed(6)})` }} className="flex relative w-[8.5in] h-[11in] origin-top-left">
+            <h1 className="text-[18pt] font-bold">{basicInfo.firstName} {basicInfo.lastName}</h1>
+        </div>
     </div>
     )
 }
