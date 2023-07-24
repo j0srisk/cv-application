be963b73824f4e310df91d2a8e2acace7e867a32
j0srisk <j0srisk@gmail.com>
2023-07-24 18:39:18 -0500

    updated resume data

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           {bullet: '• Utilized data analytics to optimize web performance and user engagement'},
           {bullet: '• Implemented front-end technologies (HTML5, CSS3, JavaScript, React) to deliver interactive user experiences'},
           {bullet: '• Drove revenue growth through proactive sales strategies, acquiring and managing a strong client base'},
-        ]
+        ],
       },
       {
         company: 'Markel Corporation',
@@ -44,9 +44,10 @@ function App() {
         endDate: 'August 2022',
         description: [
           {bullet: '• Developed a solid foundation in a large corporate setting, gaining valuable experience within a Fortune 500 company'},
-          {bullet: '• Collaborated with clients and other agencies to identify business requirements and deliver tailored web-based solutions'},
-          {bullet: '• Developed and maintained a portfolio of 10+ clients, generating $50,000+ in revenue'},
-        ]
+          {bullet: '• Presented research findings to C-suite executives, providing data-driven insights into industry trends and implications'},
+          {bullet: '• Collaborated with DevOps team to assess the implementation of emerging technologies in the surety business'},
+          {bullet: '• Analyzed various financial statements and supported underwriters in risk analysis through financial modeling techniques'},
+        ],
       },
       {
         company: 'Frontier Extraction Services',
@@ -55,10 +56,10 @@ function App() {
         startDate: 'January 2020',
         endDate: 'January 2022',
         description: [
-          {bullet: '• Developed a solid foundation in a large corporate setting, gaining valuable experience within a Fortune 500 company'},
-          {bullet: '• Collaborated with clients and other agencies to identify business requirements and deliver tailored web-based solutions'},
-          {bullet: '• Developed and maintained a portfolio of 10+ clients, generating $50,000+ in revenue'},
-        ]
+          {bullet: '• Gained valuable entrepreneurial experience through the establishment of a hemp processing and brokering venture'},
+          {bullet: '• Ensured compliance with licensing and regulatory requirements by liaising with local authorities'},
+          {bullet: '• Managed and implemented IT infrastructure, ensuring seamless business operations and data integrity'},
+        ],
       }
     ],
     projects: [
@@ -67,14 +68,52 @@ function App() {
         client: 'Slayden Grubert Beard PLLC',
         startDate: '',
         endDate: 'Fall 2022',
-        description: '• Collaborated with clients and other agencies to identify business requirements and deliver tailored web-based solutions'
+        description: [
+          {bullet: '• Developed a tailored application in collaboration with a local law firm to monitor potential conflicts of interest'},
+          {bullet: '• Played an active role in the software development life cycle, gathering and analyzing business requirements'},
+          {bullet: '• Served as a dedicated developer within an Agile team, emphasizing effective communication and iterative development'},
+          {bullet: '• Integrated third-party APIs to automate processes and seamlessly connect the custom application with existing systems'},
+        ],
       },
       {
         name: 'Book Store System Project',
         client: 'MIS 333K: Web Application Development',
         startDate: '',
         endDate: 'Spring 2022',
-        description: '• Developed a solid foundation in a large corporate setting, gaining valuable experience within a Fortune 500 company'
+        description: [
+          {bullet: '• Developed a full stack ASP.NET pseudo ecommerce site hosted on Microsoft Azure'},
+          {bullet: '• Created a robust relational database schema, optimizing table relationships for efficient data management'},
+          {bullet: '• Implemented various LINQ queries to facilitate seamless interaction with the cloud-hosted SQL relational database'},
+          {bullet: '• Utilized Git version control for effective coordination of changes and feature implementations within the development team'},
+        ],
+      },
+    ],
+    honors: [
+      {
+        name: 'Deans List',
+        date: 'Fall 2022'
+      },
+      {
+        name: 'Best in Conroe: Web Design',
+        date: 'Fall 2018'
+      },
+    ],
+    additionalInfo: [
+      {
+        name: 'Skills',
+        value: 'SQL, Python, JavaScript, HTML, CSS, Docker, Unifi, Agile Methodology, Prompt Engineering, Customer Success, MS Office'
+      },
+      {
+        name: 'Certifications',
+        value: 'In Progress - Azure Fundamentals AZ-900'
+      },
+      {
+        name: 'Interests',
+        value: 'Weightlifting, Sim Racing, Overlanding, Skiing, NCAA Football, Homelabbing, Networking'
+      },
+      {
+        name: 'Work Eligibility',
+        value: 'Eligible to work in the U.S. with no restrictions; Open to relocation'
       },
     ],
   });
