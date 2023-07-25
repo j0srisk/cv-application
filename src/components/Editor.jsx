62f548820305e7a637d9008ce86c039925ca9452
j0srisk <j0srisk@gmail.com>
2023-07-24 19:08:06 -0500

    added project component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,8 +3,44 @@ import Details from "./editors/Details";
 import Summary from "./editors/Summary";
 import Education from "./editors/Education";
 import Experience from "./editors/Experience";
+import Projects from "./editors/Projects";
 
 function Editor({ resumeData, setResumeData }) {
+
+    const handleChange = (arrayName, index, field, value, subArrayName = null, subIndex = null) => {
+        const updatedArray = [...resumeData[arrayName]];
+
+        if (subArrayName !== null && subIndex !== null) {
+          // Update a sub-item in the specified sub-array
+          updatedArray[index][field][subIndex][subArrayName] = value;
+        } else {
+          // Update a top-level field
+          updatedArray[index][field] = value;
+        }
+      
+        setResumeData({
+          ...resumeData,
+          [arrayName]: updatedArray,
+        });
+    };
+
+    const removeItem = (arrayName, index, subArrayName = null, subIndex = null) => {
+        const updatedArray = [...resumeData[arrayName]];
+        console.log(arrayName, index, subArrayName, subIndex)
+
+        if (subArrayName !== null && subIndex !== null) {
+          // Remove a sub-item
+          updatedArray[index][subArrayName].splice(subIndex, 1);
+        } else {
+          // Remove a top-level item
+          updatedArray.splice(index, 1);
+        }
+      
+        setResumeData({
+          ...resumeData,
+          [arrayName]: updatedArray,
+        });
+    };
 
     return(
         <div className="flex flex-1 flex-col gap-8 p-5 overflow-y-scroll no-scrollbar">
@@ -15,7 +51,9 @@ function Editor({ resumeData, setResumeData }) {
 
             <Education resumeData={resumeData} setResumeData={setResumeData}/>
 
-            <Experience resumeData={resumeData} setResumeData={setResumeData}/>
+            <Experience resumeData={resumeData} setResumeData={setResumeData} handleChange={handleChange} removeItem={removeItem}/>
+
+            <Projects resumeData={resumeData} setResumeData={setResumeData} handleChange={handleChange} removeItem={removeItem}/>
 
         </div>
     )
--- src/components/editors/Projects.jsx
+++ src/components/editors/Projects.jsx
@@ -1,23 +1,20 @@
 /* eslint-disable react/prop-types */
-function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
-      
-    const addExperience = () => {
-        // Create a new empty experience item
-        const newExperience = {
-            company: '',
-            title: '',
-            location: '',
+function Projects ({ resumeData, setResumeData, handleChange, removeItem }){
+
+    const addProject = () => {
+        const newProject = {
+            name: '',
+            client: '',
             startDate: '',
             endDate: '',
             description: [
-                { bullet: '' }, // You can leave an empty bullet point initially
+                { bullet: '' },
             ],
         };
-        
-        // Add the new experience to the existing experience array
+
         setResumeData({
             ...resumeData,
-            experience: [...resumeData.experience, newExperience],
+            projects: [...resumeData.projects, newProject],
         });
     };
 
@@ -30,22 +27,21 @@ function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
             [arrayName]: updatedArray,
         });
     };
-    
 
     return (
         <div className="card">
 
-            <h2 className="card-title">Experience</h2>
-            <p className="card-text">Show employers your past experience and what you have accomplished. Include simple, clear examples with action verbs to demonstrate your skills.</p>
+            <h2 className="card-title">Projects</h2>
+            <p className="card-text">List your most recent projects and achievements.</p>
 
             <div className="-mt-8">
-                {resumeData.experience.map((experienceItem, index) => (
+                {resumeData.projects.map((projectItem, index) => (
                     <div key={index} className="item">
                         <div className="flex justify-between">
-                            <h3 className="text-xl font-bold leading-7 text-gray-900"> {experienceItem.company ? experienceItem.company : "Untitled"}</h3>
+                            <h3 className="text-xl font-bold leading-7 text-gray-900"> {projectItem.name ? projectItem.name : "Untitled"}</h3>
                             <button
                                 type="button"
-                                onClick={() => removeItem('experience', index)}
+                                onClick={() => removeItem('projects', index)}
                                 className="text-sm text-red-600 font-bold">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="gray" className="w-5 h-5">
                                         <path fillRule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clipRule="evenodd" />
@@ -53,29 +49,30 @@ function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
                             </button>
                         </div>
 
+                        <div className="input-wrapper">
+                            <label htmlFor={`name${index}`} className="input-title">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                name={`name${index}`}
+                                value={projectItem.name}
+                                onChange={(e) => handleChange('projects', index, 'name', e.target.value)}
+                                className="input"
+                            />
+                        </div>
+
                         <div className="inline-wrapper">
                             <div className="input-wrapper">
-                                <label htmlFor={`company${index}`} className="input-title">
-                                    Company
+                                <label htmlFor={`client${index}`} className="input-title">
+                                    Client
                                 </label>
                                 <input
-                                    type="text"
-                                    name={`company${index}`}
-                                    value={experienceItem.company}
-                                    onChange={(e) => handleChange('experience', index, 'company', e.target.value)}
-                                    className="input"
-                                />
-                            </div>
 
-                            <div className="input-wrapper">
-                                <label htmlFor={`title${index}`} className="input-title">
-                                    Title
-                                </label>
-                                <input
                                     type="text"
-                                    name={`title${index}`}
-                                    value={experienceItem.title}
-                                    onChange={(e) => handleChange('experience', index, 'title', e.target.value)}
+                                    name={`client${index}`}
+                                    value={projectItem.client}
+                                    onChange={(e) => handleChange('projects', index, 'client', e.target.value)}
                                     className="input"
                                 />
                             </div>
@@ -83,45 +80,32 @@ function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
 
                         <div className="inline-wrapper">
                             <div className="input-wrapper">
-                                <label htmlFor={`location${index}`} className="input-title">
-                                    Location
-                                </label>
-                                <input
-                                    type="text"
-                                    name={`location${index}`}
-                                    value={experienceItem.location}
-                                    onChange={(e) => handleChange('experience', index, 'location', e.target.value)}
-                                    className="input"
-                                />
-                            </div>
-
-                            <div className="input-wrapper">
                                 <label htmlFor={`startDate${index}`} className="input-title">
-                                    Start Date
+                                    Start date
                                 </label>
                                 <input
                                     type="text"
                                     name={`startDate${index}`}
-                                    value={experienceItem.startDate}
-                                    onChange={(e) => handleChange('experience', index, 'startDate', e.target.value)}
+                                    value={projectItem.startDate}
+                                    onChange={(e) => handleChange('projects', index, 'startDate', e.target.value)}
                                     className="input"
                                 />
                             </div>
 
                             <div className="input-wrapper">
                                 <label htmlFor={`endDate${index}`} className="input-title">
-                                    End Date
+                                    End date
                                 </label>
                                 <input
                                     type="text"
                                     name={`endDate${index}`}
-                                    value={experienceItem.endDate}
-                                    onChange={(e) => handleChange('experience', index, 'endDate', e.target.value)}
+                                    value={projectItem.endDate}
+                                    onChange={(e) => handleChange('projects', index, 'endDate', e.target.value)}
                                     className="input"
                                 />
                             </div>
                         </div>
-                            
+
                         <div className="input-wrapper" >
 
                             <label htmlFor={`description${index}`} className="input-title">
@@ -129,20 +113,20 @@ function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
                             </label>
 
                             <div className="flex flex-col gap-4">
-                                {resumeData.experience[index].description.map((descriptionItem, subIndex) => (
+                                {resumeData.projects[index].description.map((descriptionItem, subIndex) => (
                                     <div key={subIndex}>
                                         <div className="flex gap-4">
                                             <input
                                                 type="text"
                                                 name={`description${subIndex}`}
                                                 value={descriptionItem.bullet}
-                                                onChange={(e) => handleChange('experience', index, 'description', e.target.value, 'bullet', subIndex)}
+                                                onChange={(e) => handleChange('projects', index, 'description', e.target.value, 'bullet', subIndex)}
                                                 className="input"
                                             />
 
-                                            {resumeData.experience[index].description.length > 1 && (
+                                            {resumeData.projects[index].description.length > 1 && (
                                                 <button 
-                                                    onClick={() => removeItem('experience', index, 'description', subIndex)}
+                                                    onClick={() => removeItem('projects', index, 'description', subIndex)}
                                                     className="input w-fit">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="gray" className="w-5 h-5">
                                                         <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
@@ -156,12 +140,13 @@ function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
                             </div>
 
                             <button 
-                                onClick={() => addBullet('experience', index)}
+                                onClick={() => addBullet('projects', index)}
                                 className="input mt-4">
                                     Add Bullet Point
                             </button>
 
                         </div>
+
                     </div>
                 ))}
             </div>
@@ -169,13 +154,13 @@ function Experience ({ resumeData, setResumeData, handleChange, removeItem }) {
             <div className="flex justify-end mt-2">
                 <button 
                     type="button"
-                    onClick={addExperience}
+                    onClick={addProject}
                     className="block w-fit rounded-md shadow-sm border-0 px-4 py-2  bg-indigo-600 text-white font-bold">
-                    Add Experience
+                    Add Project
                 </button>
             </div>
         </div>
     )
 }
 
-export default Experience;
+export default Projects;
