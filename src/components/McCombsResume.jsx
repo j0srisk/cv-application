ed0076a1534c33d9740e43846e3deb913ac7b5c0
j0srisk <j0srisk@gmail.com>
2023-07-25 23:00:18 -0500

    added text rendering properties

--- src/components/McCombsResume.jsx
+++ src/components/McCombsResume.jsx
@@ -8,52 +8,52 @@ const McCombsResume = React.forwardRef(({ resumeData }, ref) => {
     
         {/*Basic Info*/}
         <div>
-            <p className="text-[18pt] font-bold text-center font-calibri uppercase leading-[1.15]">{resumeData.firstName} {resumeData.lastName}</p>
-            <p className="text-[10.5pt] font-calibri text-center leading-[1.15]">{resumeData.email} • {resumeData.phone} • {resumeData.social}</p>
+            <p className="text-[18pt] font-bold text-center font-calibri uppercase leading-[1.15] rendering-precision">{resumeData.firstName} {resumeData.lastName}</p>
+            <p className="text-[10.5pt] font-calibri text-center leading-[1.15] rendering-precision">{resumeData.email} • {resumeData.phone} • {resumeData.social}</p>
         </div> 
     
         {/*Summary*/}
         {resumeData.summary && (
             <div>
-                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none">Summary</h2>
-                <p className="text-[10.5pt] font-calibri mt-[5.25pt] leading-[1.15]">{resumeData.summary}</p>
+                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none rendering-precision">Summary</h2>
+                <p className="text-[10.5pt] font-calibri mt-[5.25pt] leading-[1.15] rendering-legibility rendering-precision">{resumeData.summary}</p>
             </div>
         )}
     
         {/*Experience*/}
         {resumeData.experience.length > 0 && (
             <div>
-                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none">Experience</h2>
+                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none rendering-precision">Experience</h2>
                 <div className="flex flex-col mt-[5.25pt] gap-[10.5pt]">
                     {resumeData.experience.map((experience, index) => (
                         <div key={index} className="">
                             <div className="flex justify-between mb-[3.5pt]">
                                 <div className="flex gap-1">
-                                    <p className="text-[10.5pt] font-calibri font-bold leading-[1.15]">{experience.company}</p>
+                                    <p className="text-[10.5pt] font-calibri font-bold leading-[1.15] rendering-precision">{experience.company}</p>
                                     {experience.company  && experience.title &&(
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]">-</p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">-</p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri italic leading-[1.15]">{experience.title}</p>
+                                    <p className="text-[10.5pt] font-calibri italic leading-[1.15] rendering-precision">{experience.title}</p>
                                     {experience.title  && experience.location &&(
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]">-</p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">-</p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{experience.location}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{experience.location}</p>
                                 </div>
                                 
                                 <div className="flex gap-1">
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{experience.startDate}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{experience.startDate}</p>
                                     {experience.startDate && experience.endDate && (
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]"> - </p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision"> - </p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{experience.endDate}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{experience.endDate}</p>
                                 </div>
                             </div>
                             {experience.description.map((description, subIndex) => (
                                 <div key={subIndex} className="flex gap-1">
                                     {description.bullet  && (
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]">•</p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">•</p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{description.bullet}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{description.bullet}</p>
                                 </div>
                             ))}
                         </div>
@@ -66,29 +66,29 @@ const McCombsResume = React.forwardRef(({ resumeData }, ref) => {
         {/*Education*/}
         {resumeData.education.length > 0 && (
             <div>
-                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none">Education</h2>
+                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none rendering-precision">Education</h2>
                 <div className="flex flex-col gap-1 mt-[5.25pt]">
                 {resumeData.education.map((education, index) => (
                     <div key={index} className="">
                         <div className="flex gap-3">
                             <div className="flex flex-col w-1/3">
-                                <p className="text-[10.5pt] font-calibri font-bold leading-[1.15]">{education.institution}</p>
-                                <p className="text-[10.5pt] font-calibri font-bold leading-[1.15]">{education.school}</p>
+                                <p className="text-[10.5pt] font-calibri font-bold leading-[1.15] rendering-precision">{education.institution}</p>
+                                <p className="text-[10.5pt] font-calibri font-bold leading-[1.15] rendering-precision">{education.school}</p>
                             </div>
                             <div className="flex flex-col flex-1">
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{education.degree}</p>
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{education.major}</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{education.degree}</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{education.major}</p>
                                 {education.gpa && (
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">Final GPA: {education.gpa}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">Final GPA: {education.gpa}</p>
                                 )}
                             </div>
                                 
                             <div className="flex gap-1">
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{education.startDate}</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{education.startDate}</p>
                                 {education.startDate && education.endDate && (
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]"> - </p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision"> - </p>
                                 )}
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{education.endDate}</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{education.endDate}</p>
                             </div>
                         </div>
                     </div>
@@ -100,33 +100,33 @@ const McCombsResume = React.forwardRef(({ resumeData }, ref) => {
         {/*Projects*/}
         {resumeData.projects.length > 0 && (
             <div>
-                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none">Academic Projects</h2>
-                <div className="flex flex-col gap-[10.5pt] mt-[5.25pt]">
+                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none rendering-precision">Academic Projects</h2>
+                <div className="flex flex-col gap-[10.5pt] mt-[5.25pt] rendering-precision">
                     {resumeData.projects.map((project, index) => (
                         <div key={index} className="">
                             <div className="flex justify-between mb-[3.5pt]">
                                 <div className="flex gap-1">
-                                    <p className="text-[10.5pt] font-calibri font-bold leading-[1.15]">{project.name}</p>
+                                    <p className="text-[10.5pt] font-calibri font-bold leading-[1.15] rendering-precision">{project.name}</p>
                                     {project.name && project.client && (
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]"> - </p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision"> - </p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri italic leading-[1.15]">{project.client}</p>
+                                    <p className="text-[10.5pt] font-calibri italic leading-[1.15] rendering-precision">{project.client}</p>
                                 </div>
                                 
                                 <div className="flex gap-1">
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{project.startDate}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{project.startDate}</p>
                                     {project.startDate && project.endDate && (
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]"> - </p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision"> - </p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{project.endDate}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{project.endDate}</p>
                                 </div>
                             </div>
                             {project.description.map((description, subIndex) => (
                                 <div key={subIndex} className="flex gap-1">
                                     {description.bullet  && (
-                                        <p className="text-[10.5pt] font-calibri leading-[1.15]">•</p>
+                                        <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">•</p>
                                     )}
-                                    <p className="text-[10.5pt] font-calibri leading-[1.15]">{description.bullet}</p>
+                                    <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{description.bullet}</p>
                                 </div>
                             ))}
                         </div>
@@ -138,16 +138,16 @@ const McCombsResume = React.forwardRef(({ resumeData }, ref) => {
         {/*Awards*/}
         {resumeData.awards.length > 0 && (
             <div>
-                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none">Awards</h2>
+                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none rendering-precision">Awards</h2>
                 <div className="mt-[5.25pt]">
                     {resumeData.awards.map((award, index) => (
                         <div key={index}>
                             <div className="flex justify-between">
                                 <div className="flex gap-1">
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">•</p>
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{award.name}</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">•</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{award.name}</p>
                                 </div>
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{award.date}</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{award.date}</p>
                             </div>
                         </div>
                     ))}
@@ -158,13 +158,13 @@ const McCombsResume = React.forwardRef(({ resumeData }, ref) => {
         {/*Additional Information*/}
         {resumeData.additionalInfo.length > 0 && (
             <div>
-                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-[none]">Additional Information</h2>
+                <h2 className="text-[10.5pt] font-bold font-calibri uppercase border-b-[1px] border-black w-full leading-none rendering-precision">Additional Information</h2>
                 <div className="mt-[5.25pt]">
                     {resumeData.additionalInfo.map((info, index) => (
                         <div key={index}>
                             <div className="flex gap-1">
-                                <p className="text-[10.5pt] font-calibri font-bold leading-[1.15]">{info.name}:</p>
-                                <p className="text-[10.5pt] font-calibri leading-[1.15]">{info.description}</p>
+                                <p className="text-[10.5pt] font-calibri font-bold leading-[1.15] rendering-precision">{info.name}:</p>
+                                <p className="text-[10.5pt] font-calibri leading-[1.15] rendering-precision">{info.description}</p>
                             </div>
                         </div>
                     ))}
